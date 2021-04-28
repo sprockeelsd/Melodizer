51300002d611c819c71b867975a53135ca29a730
Damien <damien.sprockeels@student.uclouvain.be>
2021-04-28 15:43:17 +0200

    pour l'instant, fait juste la contrainte alldifferent mais ca fonctionne, il faut juste multiplier l'output par 100

--- Melodizer/sources/melodizer-csp.lisp
+++ Melodizer/sources/melodizer-csp.lisp
@@ -10,32 +10,33 @@
     ;the list args to be the 2nd argument of the cst-function
 )
 
-(defun post-constraint (sp cst pos drt drt* N Dn s l duration nmin nmax input)
+(defun post-constraint (sp cst N pitch)
     "Call the constraint function with the rhythm-box data and the constraint arglist."
-    (funcall (cstf cst) sp pos drt drt* N Dn s l duration nmin nmax input (args cst))
+    (funcall (cstf cst) sp N pitch (args cst))
 )
 
-; <csts> is a list of <constraint> object
-; <input> is either a voice object or a rhythm tree
-; <s> is a fraction - the shortest authorized event duration
-; <l> is a fraction - the longest authorized event duration
-; <hd> is a float between 0 and 1 that indicates the maximum rhythmic density
-;constraints note-starts note-durations chords
-(defmethod! melodizer (&optional (key 60.0) (mode 0.0))
-    ;:initvals (60.0 0.0) 
-    :indoc '("a list of constraints" "the duration of the notes that will be produced" 
-            "the starting positions of the notes that will be produced"
-             "the chords on top of which the notes have to be placed" "the key" "the mode")
+; <constraints> is a list of <constraint> object
+; <chords> is a chord sequence
+; <note-starts> is a list of integers representing the start of each note
+; <note-durations> is a list of integers representing the duration of each note
+; <key> is the key in which the melody is
+; <mode> is the mode the melody is in (major, minor)
+; add constraints to signature later
+(defmethod! melodizer ( chords note-starts note-durations &optional (key 60.0) (mode 0.0))
+    :initvals (list (make-instance 'chord-seq) nil nil 60.0 0.0)
+    :indoc '("a list of constraints" "the chords on top of which notes have to be placed" 
+            "the starting position of the notes that will be produced" 
+            "the durations of the notes that will be produced"
+            "the key" 
+            "the mode"
+            )
     :icon 921
-    :doc "
-TODO : add documentation once the code has been modified
-"
-    (let ((sp (new-space))
-        (N (get-N note-starts))
+    :doc "TODO : add documentation once the code does something interesting"
+    (let ((sp (gil::new-space))
         pitch dfs)
 
         ;first, create the variables
-        (setq pitch (gil::add-int-var-array sp N 21 108))
+        (setq pitch (gil::add-int-var-array sp 10 90 108))
 
         ;then, post the constraints
         (gil::g-distinct sp pitch)
@@ -47,7 +48,7 @@ TODO : add documentation once the code has been modified
         (setq se (gil::search-engine sp nil))
 
         ;return
-        (list pitch)
+        (list se pitch note-starts note-durations)
     )
 )
 
@@ -59,29 +60,23 @@ TODO : add documentation once the code has been modified
 Get the next solution for the rhythm csp described in the input rhythm-space.
 "
     (let ((se (first l))
-         (drt* (second l))
-         (input (third l))
-         (s* (nth 3 l))
-         sol drt rtree)
+         (pitch* (second l))
+         (starts (third l))
+         (durations (fourth l))
+         sol pitches rtree)
         
-        ;Get the values of the solution and build the tree
+        ;Get the values of the solution
         (setq sol (gil::search-next se))
         (if (null sol) (error "No solution or no more solution."))
-        (setq drt (gil::g-values sol drt*))
-        (if (typep input 'voice)
-            (setq rtree (build-rtree (tree input) drt s*))
-            (setq rtree (build-rtree input drt s*)))
+        (setq pitches (gil::g-values sol pitch*))
 
-        ;return a voice object based on the input
-        (if (typep input 'voice)
-            (make-instance 'voice
-                :tree rtree
-                :chords (chords input)
-                :tempo (tempo input)
-                :legato (legato input)
-                :ties (ties input))
-            (make-instance 'voice
-                :tree rtree
-                :chords '((7200)))
+
+        ;return a list of integers with the solution
+        (make-instance 'chord-seq
+            :lmidic pitches
+            :lonset starts
+            :ldur durations
+        )
+
     )
-))
+)
