3286f03c53c5d7ec2143fa3951f3c385416d7e7c
Damien <damien.sprockeels@student.uclouvain.be>
2021-09-17 10:13:38 +0200

    melody finder object can now access its inputs

--- Melodizer/sources/melody-finder.lisp
+++ Melodizer/sources/melody-finder.lisp
@@ -5,7 +5,10 @@
 (in-package :om)
 
 (om::defclass! melody-finder () 
-  ((input-chords :accessor input-chords :initarg :init-chords :initform (make-instance 'voice) :documentation "the input chords on top of which the melody will be played")
+  ((input-chords :accessor input-chords :initarg :input-chords :initform (make-instance 'voice) :documentation "the input chords on top of which the melody will be played")
+    (search-engine :accessor search-engine :initarg :search-engine :initform nil :documentation "search engine for the CSP")
+    (solution :accessor solution :initarg :solution :initform nil :documentation "solution of the CSP")
+    (slot2 :accessor slot2 :initarg :slot2 :initform nil :documentation "slot 2")
     ;(slot2 :accessor slot2 :initarg :slot2 :initform nil :documentation "slot 2")
   )
   (:icon 1)
@@ -31,6 +34,8 @@
 
 
 (defmethod initialize-instance ((self my-editor) &rest args)
+
+  ; To access the melody-finder object, (object self)
   
   ;;; do what needs to be done by default
   (call-next-method)
@@ -52,12 +57,23 @@
       (om::om-make-point 100 10) ; position
       (om::om-make-point 80 20) ; size
       "Restart"
-      :di-action #'(lambda (b)
-                   (print "Restarting the search with the new constraints"))
+      :di-action #'(lambda (b) 
+                    (dolist (e (chords (input-chords (object self))))
+                      (print (lmidic e))
+                    )
+                  )
+      ;:di-action #'(lambda (b)
+      ;             (print "Restarting the search with the new constraints"))
     )
   )
   ; return the editor:
   self
+)
+
+(defmethod get-slot-2 ((self melody-finder)) (slot2 self))
+
+(defmethod! simple-method (finder)
+  (print (slot2 finder))
 )
 
 
