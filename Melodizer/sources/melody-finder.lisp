e3efa2ed7c90e8c1a191069aa8c33808ec45c30f
Damien <damien.sprockeels@student.uclouvain.be>
2021-09-22 15:53:19 +0200

    the object melody-finder now works, it can create the CSP and search for solutions

--- Melodizer/sources/melody-finder.lisp
+++ Melodizer/sources/melody-finder.lisp
@@ -8,7 +8,8 @@
   ;attributes
   ((input-chords :accessor input-chords :initarg :input-chords :initform (make-instance 'voice) :documentation "the input chords on top of which the melody will be played")
     (search-engine :accessor search-engine :initarg :search-engine :initform nil :documentation "search engine for the CSP")
-    (solution :accessor solution :initarg :solution :initform nil :documentation "solution of the CSP")
+    (solution :accessor solution :initarg :solution :initform nil :documentation "solution of the CSP in the form of a voice object")
+    (cspsol :accessor cspsol :initarg :cspsol :initform nil :documentation "solution of the CSP")
     (slot2 :accessor slot2 :initarg :slot2 :initform nil :documentation "slot 2") ; to do some testing
     ;(slot2 :accessor slot2 :initarg :slot2 :initform nil :documentation "slot 2")
   )
@@ -45,6 +46,27 @@
   
   (om::om-add-subviews 
     self
+    ; button to start or restart the search, not sure if I will keep it here
+    (om::om-make-dialog-item 
+      'om::om-button
+      (om::om-make-point 100 10) ; position
+      (om::om-make-point 80 20) ; size
+      "Start"
+      :di-action #'(lambda (b) 
+                    #| (dolist (e (chords (input-chords (object self))))
+                      (print (lmidic e))
+                    ) |#
+                    (let init 
+                      (setq init (voicemelodizer (input-chords (object self)))); get the search engine and the first solution of the CSP
+                      ; update the fields of the object to their new value
+                      (setf (search-engine (object self)) (first init))
+                      (setf (cspsol (object self)) (second init))
+                      (print "csp constructed")
+                    )
+                  )
+      ;:di-action #'(lambda (b)
+      ;             (print "Restarting the search with the new constraints"))
+    )
     ; button to find the next solution
     (om::om-make-dialog-item 
       'om::om-button
@@ -52,27 +74,83 @@
       (om::om-make-point 80 20) ; size
       "Next"
       :di-action #'(lambda (b)
-                   (print "Searching for the next solution"))
-                   ;do something (search-next from melodizer-csp to modify so it works here)
-    )
-    ; button to start or restart the search, not sure if I will keep it here
-    (om::om-make-dialog-item 
-      'om::om-button
-      (om::om-make-point 100 10) ; position
-      (om::om-make-point 80 20) ; size
-      "Restart"
-      ; for now, it doesn't do that
-      :di-action #'(lambda (b) 
-                    (dolist (e (chords (input-chords (object self))))
-                      (print (lmidic e))
+                    (print "Searching for the next solution")
+                    (let sol 
+                      (setf (solution (object self)) (search-next-voice (list (search-engine (object self)) (cspsol (object self)))))
                     )
                   )
-      ;:di-action #'(lambda (b)
-      ;             (print "Restarting the search with the new constraints"))
+                    
+                   ;(search-next-voice (list (search-engine (object self) (cspsol (object self)))))
+                   ;do something (search-next from melodizer-csp to modify so it works here)
     )
   )
   ; return the editor:
   self
+)
+
+(defmethod voicemelodizer ( input &optional (key 60.0) (mode 0.0))
+    :initvals (list (make-instance 'voice) 60.0 0.0)
+    :indoc '("a voice object" 
+            "the key" 
+            "the mode"
+            )
+    :icon 921
+    :doc "TODO : add documentation once the code does something interesting"
+    (let ((sp (gil::new-space))
+        pitch dfs)
+
+        ; first, create the variables
+        (setq pitch (gil::add-int-var-array sp 8 60 72))
+
+        ; then, post the constraints
+        (in-tonality sp pitch 60 0)
+
+        ;(all-different-notes sp pitch)
+
+        ;(interval-between-adjacent-notes sp pitch)
+        
+        ; branching
+        ; in order branching
+        (gil::g-branch sp pitch 0 0)
+        ;random branching
+        ;(gil::g-branch-random sp pitch 1 2)
+
+        ; search engine
+        (setq se (gil::search-engine sp nil))
+
+        ; return
+        (list se pitch)
+    )
+)
+
+(defmethod search-next-voice (l)
+    :initvals (list nil) 
+    :indoc '("a musical-space")
+    :icon 330
+    :doc "
+Get the next solution for the csp described in the input musical-space.
+"
+    (let ((se (first l))
+         (pitch* (second l))
+         sol pitches)
+        
+        ;Get the values of the solution
+
+        (setq sol (gil::search-next se))
+        (if (null sol) (error "No solution or no more solution."))
+        ;(print gil::g-values pitch*)
+        (setq pitches (to-midicent (gil::g-values sol pitch*)))
+        (print "pitches" )
+        (print pitches)
+
+        ;return a voice object
+        (make-instance 'voice
+            :tree (mktree (list 1/4 1/4 1/4 1/4 1/4 1/4 1/4 1/4) (list 4 4))
+            :chords pitches
+            :tempo 72
+        )
+        ;(list (mktree (list 1/4 1/4 1/4 1/4 1/4 1/4 1/4 1/4) (list 4 4)) pitches 72)
+    )
 )
 
 ;(defmethod get-slot-2 ((self melody-finder)) 
