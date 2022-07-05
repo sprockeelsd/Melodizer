85dc2d18deb92badf94d5dd2384c05b5f6e654c6
Damien Sprockeels <damien.sprockeels@student.uclouvain.be>
2022-07-05 10:43:31 +0200

    revert to version 1.0 to make the git cleaner

--- Melodizer_v2.0/Melodizer-main/Melodizer/sources/package.lisp
+++
@@ -1,8 +0,0 @@
-(in-package :om)
-
-(defvar *MELODIZER-path* (make-pathname  :directory (append (pathname-directory *load-pathname*) (list "MELODIZER"))))
-
-(require-library "GIL")
-
-(defpackage :mldz
-(:use "COMMON-LISP" "OM" "CL-USER"))
